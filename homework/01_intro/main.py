b7e23296a9d6d92e9a82836c88fcf343754c3e26
Olia <oliaopanasiukk@gmail.com>
2024-12-06 01:01:15 +0200

    update hw1

--- homework/01_intro/main.py
+++ homework/01_intro/main.py
@@ -1,30 +1,36 @@
-# task 1
-fruits = ('pineapple', 'grape', 'berry', 'cherry', 'kiwi', 'grape', 'strawberry', 'grape')
-userInput = input('Enter a fruit: ')
-print(f'{userInput} count -> {fruits.count(userInput)}' )
+# # task 1
+# fruits = ('pineapple', 'grape', 'berry', 'cherry', 'kiwi', 'grape', 'strawberry', 'grape')
+# userInput = input('Enter a fruit: ')
+# print(f'{userInput} count -> {fruits.count(userInput)}' )
 
-# task 2
-count = 0
+# # task 2
+# count = 0
 
-for x in fruits:
-    if userInput in x: count += 1
+# for x in fruits:
+#     if userInput in x: count += 1
 
-print(userInput, 'parts ->', count, "\n")
+# print(userInput, 'parts ->', count, "\n")
 
-# task 3
-cars = ['lexus', 'audi', 'toyota', 'honda', 'audi', 'nissan', 'lexus']
-userInput = input('Enter a manufacture: ')
-toReplace = input('Enter a word to replace: ')
+# # task 3
+# cars = ['lexus', 'audi', 'toyota', 'honda', 'audi', 'nissan', 'lexus']
+# userInput = input('Enter a manufacture: ')
+# toReplace = input('Enter a word to replace: ')
 
-for c in range(len(cars)):
-    if cars[c] == userInput: cars[c] = toReplace
+# for c in range(len(cars)):
+#     if cars[c] == userInput: cars[c] = toReplace
 
-print(cars, "\n")
+# print(cars, "\n")
 
-# task 4
+# # task 4
 tuple = (5, 48, 665, 54, 12, 7, 6514)
 sortedTuple = sorted(tuple)  
+number = {}
 
 for i in sortedTuple:
     count = len(str(i))
     print(f'{i} - has {count} digit(s)')
+    if count in number: number[count] += 1 
+    else: number[count] = 1 
+
+for i, count in number.items():
+    print(f'{i} occurs: {count} times')
